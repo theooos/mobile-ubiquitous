f565c2b3c7b70ffe0f17b8d2b216d963208c709a
Theo Gregory <tjmg@outlook.com>
2018-02-25 22:33:57 +0000

    Main menu design made

--- RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/ResultsActivity.java
+++ RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/ResultsActivity.java
@@ -3,11 +3,11 @@ package red.theodo.restauranthygienechecker1453831;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_results);
     }
 }
