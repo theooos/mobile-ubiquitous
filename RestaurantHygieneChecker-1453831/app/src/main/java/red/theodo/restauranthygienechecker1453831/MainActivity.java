7d3977cd4e9a6952e94dce6a38b96561b0d7197b
Theo Gregory <tjmg@outlook.com>
2018-03-06 19:31:52 +0000

    Bottom nav

--- RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/MainActivity.java
+++ RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/MainActivity.java
@@ -1,13 +1,26 @@
 package red.theodo.restauranthygienechecker1453831;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String SEARCH_STRING = "search";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void performSearch(View view){
+        Intent intent = new Intent(this, ResultsActivity.class);
+        EditText editText = findViewById(R.id.search_text);
+        String message = editText.getText().toString();
+        intent.putExtra(SEARCH_STRING, message);
+        startActivity(intent);
     }
 }
--- RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/ResultsActivity.java
+++ RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/ResultsActivity.java
@@ -1,13 +1,41 @@
 package red.theodo.restauranthygienechecker1453831;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_list:
+                    mTextMessage.setText(R.string.title_list);
+                    return true;
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
