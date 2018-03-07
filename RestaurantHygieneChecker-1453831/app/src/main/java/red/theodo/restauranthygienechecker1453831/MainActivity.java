51b2296e0c6e86a01ba04892836e77f5bba5dc1f
Theo Gregory <tjmg@outlook.com>
2018-03-07 18:04:00 +0000

    Adds advanced menu with toggle

--- RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/MainActivity.java
+++ RestaurantHygieneChecker-1453831/app/src/main/java/red/theodo/restauranthygienechecker1453831/MainActivity.java
@@ -2,6 +2,7 @@ package red.theodo.restauranthygienechecker1453831;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
@@ -9,11 +10,25 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     public static String SEARCH_STRING = "search";
+    private boolean advanced = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.advancedContainer).setVisibility(ConstraintLayout.GONE);
+        findViewById(R.id.refineButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(advanced){
+                    findViewById(R.id.advancedContainer).setVisibility(ConstraintLayout.GONE);
+                    advanced = false;
+                } else {
+                    findViewById(R.id.advancedContainer).setVisibility(ConstraintLayout.VISIBLE);
+                    advanced = true;
+                }
+            }
+        });
     }
 
     public void performSearch(View view) {
